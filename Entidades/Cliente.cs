1e2da21866c10af83df87328befad674bdf3f5ce
Francisco Flores <francisco.giavay@gmail.com>
2026-09-15 19:22:41 -0300

    Agregue clase Cliente

---
+++ Entidades/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Cliente
+    {
+        private int id {get; set;}
+        string nombre;
+        int dni;
+        int tel;
+        string email;
+        string direccion;
+
+        public Cliente(string nombre, int dni, int tel, string email, string direccion)
+        {            
+            this.nombre = nombre;
+            this.dni = dni;
+            this.tel = tel;
+            this.email = email;
+            this.direccion = direccion;
+        }
+    }
+
+}
