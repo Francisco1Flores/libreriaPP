5eead4ac3117a5659179c62f5114658af5f64ddc
Francisco Flores <francisco.giavay@gmail.com>
2026-09-15 19:12:37 -0300

    Agregar archivos de proyecto.

---
+++ Datos/ConexionDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace Datos
+{
+    public class ConexionDB
+    {        
+        private static string cadenaConexion = "Server=localhost;Port=3306;Database=libreriadb;UserID=root;Password=;";
+
+        public static MySqlConnection ObtenerConexion()
+        {
+            return new MySqlConnection(cadenaConexion);
+        }
+    }
+}
---
+++ LibreriaPP.Presenteacion/FormPrincipal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Datos;
+using MySqlConnector;
+
+namespace Presenteacion
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection conexion = ConexionDB.ObtenerConexion();
+                conexion.Open();
+                MessageBox.Show("conexion exitosa");
+
+            } catch (Exception ex)
+            {
+                MessageBox.Show("error: " + ex.Message);
+            }
+        }
+    }
+}
